13b6f643385fe4188d520a7efb91b3266a122d03
Jesse Nieminen <jesse.nieminen@intel.com>
2017-05-22 09:38:59 +0300

    v5

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
 console.log(5+6);
+var output = document.querySelector('.output');
+var dataArray = [];
+var i = dataArray.length;      //index to track amount of data points
 
 function get_click(buttonID)    //ID not necessarily numerical
 {
         console.log(buttonID);
+        dataArray.push(buttonID);        
+        store('dataArray', dataArray);
+        console.log(retrieve('dataArray'));
+        console.log(buttonID.length);
+        if(window.deviceOrientationEvent)
+        {
+	        window.addEventListener('deviceorientation', function(event)
+                        {
+                                console.log('Alpha(x): ' + event.alpha +  'Beta(y): ' + event.beta + 'Gamma(z): ' + event.gamma);
+                                output.innerHTML  = "beta : " + event.beta + "\n";
+                                output.innerHTML += "gamma: " + event.gamma + "\n";
+                                //data = ...;
+                        }
+	        );
+        }
+        if(window.DeviceMotionEvent)
+        {
+	        window.addEventListener('devicemotion', function(event)
+		        {
+			        var acceleration = event.acceleration;
+          		        var rotationRate = event.rotationRate;
+		                var gacc = event.accelerationIncludingGravity;
+		          
+		                console.log(acceleration.x + ' : ' + acceleration.y + ' : ' + acceleration.z);
+                                console.log(event.acceleration + ' : ' + event.rotationRate + ' : ' + event.interval);
+                                console.log(rotationRate.alpha + ' : ' + rotationRate.beta + ' : ' + rotationRate.gamma);
+                                //console.log(gacc.x + ' : ' + gacc.y + ' : ' + gacc.z);
+                                //data = ...                        
+		        }
+	        );
+
+        }
 }
 
-function store (data)
+function store (key, data)   //currently uses LocalStorage, maybe should use something else?
 {
         if (typeof(Storage) !== "undefined") 
         {
-                if ( typeof val === "undefined" ) 
-                {
-                        var value = this.getItem(key);
-                        return value ? JSON.parse(value) : null;
-                }
-                else
-                {
-                        localStorage.setItem('data', JSON.stringify(data));
-                }
+                        localStorage.setItem(key, JSON.stringify(data));
             
         } 
         else 
@@ -26,32 +53,9 @@ function store (data)
         }
 }
 
-var output = document.querySelector('.output');
-if(window.deviceOrientationEvent)
+function retrieve (key)
 {
-	window.addEventListener('deviceorientation', function(event)
-                {
-                        console.log('Alpha(x): ' + event.alpha +  'Beta(y): ' + event.beta + 'Gamma(z): ' + event.gamma);
-  output.innerHTML  = "beta : " + event.beta + "\n";
-  output.innerHTML += "gamma: " + event.gamma + "\n";
-                }
-	);
+        return JSON.parse(localStorage.getItem(key));
 }
-if(window.DeviceMotionEvent)
-{
-	window.addEventListener('devicemotion', function(event)
-		{
-			var acceleration = event.acceleration;
-  		        var rotationRate = event.rotationRate;
-		        var gacc = event.accelerationIncludingGravity;
-		  
-		        console.log(acceleration.x + ' : ' + acceleration.y + ' : ' + acceleration.z);
-                        console.log(event.acceleration + ' : ' + event.rotationRate + ' : ' + event.interval);
-                        console.log(rotationRate.alpha + ' : ' + rotationRate.beta + ' : ' + rotationRate.gamma);
-                        //console.log(gacc.x + ' : ' + gacc.y + ' : ' + gacc.z);
-                        //data = ...                        
-		}
-	);
 
-}
-//store(data):
+//store(data);
