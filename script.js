e5c7dabfda9cd588a68e6b596d6f319aa868124b
Jesse Nieminen <jesse.nieminen@intel.com>
2017-05-23 14:12:00 +0300

    v143

--- script.js
+++ script.js
@@ -232,7 +232,7 @@ function read_sensors()
                                 //accel = {x:1.1, y:2.2, z: 7.7}  //TESTI
                                 //console.log(newAccel)
                                 gravity.update(sensors.Accelerometer);
-                                //gravity.normalize();    //To do this or to not do this..?
+                                gravity.normalize();    //To do this or to not do this..?
                                 if (!(isNaN(gravity.x) && isNaN(gravity.y) && isNaN(gravity.z)))      //to prevent NaN
                                 {
                                         accelNoG = {x:accel.x - gravity.x, y:accel.y - gravity.y, z:accel.z - gravity.z}
