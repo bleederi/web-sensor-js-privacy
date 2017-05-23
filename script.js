9aaa0d4fcac33713ea6f09ab2c8bc5af9667dd63
Jesse Nieminen <jesse.nieminen@intel.com>
2017-05-23 14:41:40 +0300

    v150

--- script.js
+++ script.js
@@ -275,6 +275,7 @@ function read_sensors()
                         sensors.AbsoluteOrientationSensor.onchange = event => {
                         sensors.AbsoluteOrientationSensor.populateMatrix(orientationMat);
                         //console.log("Orientation matrix: " + orientationMat);
+                                        document.getElementById("ori").textContent = `Orientation matrix ${orientationMat}`;
                       }
                       sensors.Gyroscope.onchange = event => {
                         var xVelGyro = sensors.Gyroscope.x;
