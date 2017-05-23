7b98fd81895679dbacf2e0f8c8f41e2062c283c7
Jesse Nieminen <jesse.nieminen@intel.com>
2017-05-23 13:27:07 +0300

    v124

--- script.js
+++ script.js
@@ -226,14 +226,15 @@ function read_sensors()
 */
               sensors.Accelerometer.onchange = event => {
                 accel = {x:sensors.Accelerometer.x, y:sensors.Accelerometer.y, z:sensors.Accelerometer.z};
-                        let newAccel = accel;
+                        //let newAccel = accel;
                         //accel = {x:1.1, y:2.2, z: 7.7}  //TESTI
                         //console.log(newAccel)
                         gravity.update(sensors.Accelerometer);
-                        //gravity.normalize();
+                        gravity.normalize();
                         if (!(isNaN(gravity.x) && isNaN(gravity.y) && isNaN(gravity.z)))      //to prevent NaN
                         {
                                 accelNoG = {x:accel.x - gravity.x, y:accel.y - gravity.y, z:accel.z - gravity.z}
+                                console.log(`Isolated gravity (${gravity.x}, ${gravity.y}, ${gravity.z})`);
                         }
                         else
                         {
