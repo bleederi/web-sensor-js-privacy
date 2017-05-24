3059dc6727aaf492866cec36cc134683e4f2e436
Jesse Nieminen <jesse.nieminen@intel.com>
2017-05-24 12:13:55 +0300

    v209, work on data sending feature

--- script.js
+++ script.js
@@ -243,3 +243,22 @@ function read_sensors() //ran when a button is pressed, saves data gathered from
                 //console.log("Orientation matrix: " + orientationMat);
         }
 }
+
+//TODO: Send data to a server to be processed
+function send_data()    //https://stackoverflow.com/a/24468752
+{
+// Sending and receiving data in JSON format using POST mothod
+//
+var xhr = new XMLHttpRequest();
+var url = "url";
+xhr.open("POST", url, true);
+xhr.setRequestHeader("Content-type", "application/json");
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        var json = JSON.parse(xhr.responseText);
+        console.log(json.email + ", " + json.password);
+    }
+};
+var data = JSON.stringify({"email": "hey@mail.com", "password": "101010"});
+xhr.send(data);
+}
