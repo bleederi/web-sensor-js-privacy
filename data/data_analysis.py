8bcc9272ba0d66155043fa1d398c382f05959d90
Jesse Nieminen <jesse.nieminen@intel.com>
2017-05-26 17:17:45 +0300

    v230, more work on data analysis, added new data

--- data/data_analysis.py
+++ data/data_analysis.py
@@ -5,45 +5,33 @@ from collections import OrderedDict
 
 def convert_orientation(jsondata):      #Convert orientation matrix to Euler angles
         orientationjsondata = []        #New JSON data with orientation converted to Euler angles
-        for buttonpress in jsondata[1:2]:
+        for buttonpress in jsondata:
                 if(buttonpress['button'] !=  None):     #skip bad values
                         orientationdict = {}      #Dict (JSON data) for a button press with orientation converted
                         for key, value in buttonpress.items():
                                 if(key != 'orientation'):
                                         orientationdict[key] = value
                                 else:   #Here convert orientation
+                                        orientationdict[key] = []
                                         for orimatrix in value:  #Each orientation matrix
+                                                tempdict = {}   #Temp dict to store Euler angle values
                                                 #print(orivalue)
                                                 r11 = orimatrix['0']
                                                 r21 = orimatrix['4']
                                                 r31 = orimatrix['8']
                                                 r32 = orimatrix['9']
                                                 r33 = orimatrix['10']
-                                                print(r11, r21, r31, r32, r33)
-                                                alpha = math.atan2(r21, r11)
-                                                print(alpha)
-        #beta = atan2(-r31/(math.sqrt(math.pow(r32,2) + math.pow(r33,2))))
-        #gamma = atan(r32/r33)
-        #R = orientation matrix
-        """
-        assert(isRotationMatrix(R))
-
-        sy = math.sqrt(R[0,0] * R[0,0] +  R[1,0] * R[1,0])
-
-        singular = sy < 1e-6
-
-        if  not singular :
-        x = math.atan2(R[2,1] , R[2,2])
-        y = math.atan2(-R[2,0], sy)
-        z = math.atan2(R[1,0], R[0,0])
-        else :
-        x = math.atan2(-R[1,2], R[1,1])
-        y = math.atan2(-R[2,0], sy)
-        z = 0
-
-        return np.array([x, y, z])
-        """
-        return jsondata
+                                                #print(r11, r21, r31, r32, r33)
+                                                betadivisor = math.sqrt(math.pow(r32,2) + math.pow(r33,2))
+                                                if(r11 != 0 and r33 != 0 and betadivisor != 0): #Can't divide by zero
+                                                        alpha = math.atan2(r21, r11)
+                                                        beta = math.atan2(-r31, (math.sqrt(math.pow(r32,2) + math.pow(r33,2))))
+                                                        gamma = math.atan2(r32, r33)
+                                                        #print(alpha, beta, gamma)
+                                                tempdict = {'alpha': alpha, 'beta': beta, 'gamma': gamma}
+                                                orientationdict['orientation'].append(tempdict)
+                orientationjsondata.append(orientationdict)
+        return orientationjsondata
 def remove_initial(jsondata):   #Do operations on JSON formatted data
         cancelledjsondata = [] #New JSON data with the effect of initial position and orientation cancelled
         for buttonpress in jsondata:
@@ -56,31 +44,22 @@ def remove_initial(jsondata):   #Do operations on JSON formatted data
                                 if key != 'button' and key != 'frequency':
                                         i = 0
                                         cancelleddict[key] = []
+                                        while(value[i] == None):
+                                                i = i + 1
+                                        initialvalue = value[i]
                                         if(key == 'acceleration'):
-                                                while(value[i] == None):
-                                                        i = i + 1
-                                                initialvalue = value[i]
                                                 for accvalue in value:
                                                         acc_c = {key: accvalue[key] - initialvalue.get(key, 0) for key in accvalue.keys()}
                                                         cancelleddict[key].append(acc_c)
                                         if(key == 'accelerationnog'):
-                                                while(value[i] == None):
-                                                        i = i + 1
-                                                initialvalue = value[i]
                                                 for accnogvalue in value:
                                                         accnog_c = {key: accnogvalue[key] - initialvalue.get(key, 0) for key in accnogvalue.keys()}
                                                         cancelleddict[key].append(accnog_c)
                                         if(key == 'orientation'):
-                                                while(value[i] == None):
-                                                        i = i + 1
-                                                initialvalue = value[i]
                                                 for orientationvalue in value:
                                                         orientationd = {key: orientationvalue[key] - initialvalue.get(key, 0) for key in orientationvalue.keys()}
                                                         cancelleddict[key].append(orientationd)           
                                         if(key == 'rotation'):
-                                                while(value[i] == None):
-                                                        i = i + 1
-                                                initialvalue = value[i]
                                                 for rotationvalue in value[i:]:
                                                         rotationd = {key: rotationvalue[key] - initialvalue.get(key, 0) for key in rotationvalue.keys()}
                                                         cancelleddict[key].append(rotationd)           
@@ -127,54 +106,140 @@ def calc_dac(jsondata): #Function to calculate DAC of acceleration data and add 
                                                         dac = math.sqrt(math.pow(accvalue['x']-previous['x'], 2) + math.pow(accvalue['y']-previous['y'], 2) + math.pow(accvalue['z']-previous['z'], 2))
                                                 previous = accvalue
                                                 dacdict['dac'].append(dac)
-                                                #print("Added ", dac)
                         jsondata_withdac.append(dacdict)
         return jsondata_withdac
 
 def calc_stats(jsondata):       #Function to obtain statistical features of the button press data
+        statsdict = []      #List for the statistics of a button press
         for buttonpress in jsondata:
                 if(buttonpress['button'] !=  None):     #skip bad values
-                        statsdict = {}      #Dict (JSON data) for the statistics of a button press
+                        buttonstats = {}        #Dict of all statistics related to one button press
                         for key, value in buttonpress.items():
+                                if key == 'button' or key == 'frequency':
+                                        buttonstats[key] = value
                                 if key != 'button' and key != 'frequency':
                                         #Calculate maximums
-                                        statsdict[key + '_max'] = []
-                                        for i in value:
-                                                maxdict = {}    #Dict to store max values of a single sequence
-                                                #print(i)
+                                        buttonstats[key + '_max'] = []
+                                        #print(key, value)
+                                        if (key == 'acceleration' or key == 'accelerationnog' or key == 'rotation' or key == 'acceleration_d' or key == 'accelerationnog_d' or key == 'rotation_d'):
+                                                #print(value)                                                        
+                                                max_x = value[0]['x']
+                                                max_y = value[0]['y']
+                                                max_z = value[0]['z']
+                                                for i in value: #i is a sensor reading
+                                                        if(value[value.index(i)]['x'] > max_x):
+                                                                max_x = value[value.index(i)]['x']
+                                                        if(value[value.index(i)]['y'] > max_y):
+                                                                max_y = value[value.index(i)]['y']
+                                                        if(value[value.index(i)]['z'] > max_z):
+                                                                max_z = value[value.index(i)]['z']
+                                                maxdict = {'x':max_x, 'y': max_y, 'z': max_z}    #Dict to store max values of a single sequence
+                                        #Need to handle orientation and DAC separately (different structure)
+                                        if (key == 'orientation' or key == 'orientation_d'):
+                                                max_alpha = value[0]['alpha']
+                                                max_beta = value[0]['beta']
+                                                max_gamma = value[0]['gamma']
+                                                for i in value: #i is a sensor reading
+                                                        if(value[value.index(i)]['alpha'] > max_alpha):
+                                                                max_alpha = value[value.index(i)]['alpha']
+                                                        if(value[value.index(i)]['beta'] > max_beta):
+                                                                max_beta = value[value.index(i)]['beta']
+                                                        if(value[value.index(i)]['gamma'] > max_gamma):
+                                                                max_gamma = value[value.index(i)]['gamma']
+                                                maxdict = {'alpha':max_alpha, 'beta': max_beta, 'gamma': max_gamma}    #Dict to store max values of a single sequence
+                                        if (key == 'dac'):
+                                                max_dac = max(value)
+                                                maxdict = {'dac':max_dac}    #Dict to store max values of a single sequence
+                                        buttonstats[key + '_max'].append(maxdict)
+                                        #Calculate minimums
+                                        buttonstats[key + '_min'] = []
+                                        if (key == 'acceleration' or key == 'accelerationnog' or key == 'rotation' or key == 'acceleration_d' or key == 'accelerationnog_d' or key == 'rotation_d'):                                                      
+                                                min_x = value[0]['x']
+                                                min_y = value[0]['y']
+                                                min_z = value[0]['z']
+                                                for i in value: #i is a sensor reading
+                                                        if(value[value.index(i)]['x'] < min_x):
+                                                                min_x = value[value.index(i)]['x']
+                                                        if(value[value.index(i)]['y'] < min_y):
+                                                                min_y = value[value.index(i)]['y']
+                                                        if(value[value.index(i)]['z'] < min_z):
+                                                                min_z = value[value.index(i)]['z']
+                                                mindict = {'x':min_x, 'y': min_y, 'z': min_z}    #Dict to store min values of a single sequence
+                                        #Need to handle orientation and DAC separately (different structure)
+                                        if (key == 'orientation' or key == 'orientation_d'):
+                                                min_alpha = value[0]['alpha']
+                                                min_beta = value[0]['beta']
+                                                min_gamma = value[0]['gamma']
+                                                for i in value: #i is a sensor reading
+                                                        if(value[value.index(i)]['alpha'] < min_alpha):
+                                                                min_alpha = value[value.index(i)]['alpha']
+                                                        if(value[value.index(i)]['beta'] < min_beta):
+                                                                min_beta = value[value.index(i)]['beta']
+                                                        if(value[value.index(i)]['gamma'] < min_gamma):
+                                                                min_gamma = value[value.index(i)]['gamma']
+                                                mindict = {'alpha':min_alpha, 'beta': min_beta, 'gamma': min_gamma}    #Dict to store min values of a single sequence
+                                        if (key == 'dac'):
+                                                min_dac = min(value)
+                                                mindict = {'dac':min_dac}    #Dict to store min values of a single sequence
+                                        buttonstats[key + '_min'].append(mindict)
+                                        #Calculate averages
+                                        buttonstats[key + '_avg'] = []
+                                        if (key == 'acceleration' or key == 'accelerationnog' or key == 'rotation' or key == 'acceleration_d' or key == 'accelerationnog_d' or key == 'rotation_d'):                                                      
+                                                sum_x = value[0]['x']
+                                                sum_y = value[0]['y']
+                                                sum_z = value[0]['z']
+                                                for i in value: #i is a sensor reading
+                                                                sum_x = sum_x + value[value.index(i)]['x']
+                                                                sum_y = sum_y + value[value.index(i)]['y']
+                                                                sum_z = sum_z + value[value.index(i)]['z']
+                                                avgdict = {'x':sum_x/len(value), 'y': sum_y/len(value), 'z': sum_z/len(value)}    #Dict to store min values of a single sequence
+                                        #Need to handle orientation and DAC separately (different structure)
+                                        if (key == 'orientation' or key == 'orientation_d'):
+                                                sum_alpha = value[0]['alpha']
+                                                sum_beta = value[0]['beta']
+                                                sum_gamma = value[0]['gamma']
+                                                for i in value: #i is a sensor reading
+                                                                sum_alpha = sum_alpha + value[value.index(i)]['alpha']
+                                                                sum_beta = sum_beta + value[value.index(i)]['beta']
+                                                                sum_gamma = sum_gamma + value[value.index(i)]['gamma']
+                                                avgdict = {'alpha':sum_alpha/len(value), 'beta': sum_beta/len(value), 'gamma': sum_gamma/len(value)}    #Dict to store min values of a single sequence
+                                        if (key == 'dac'):
+                                                sum_dac = sum(value)/len(value)
+                                                avgdict = {'dac':sum_dac}    #Dict to store avg values of a single sequence
+                                        buttonstats[key + '_avg'].append(avgdict)
+                        statsdict.append(buttonstats)
         return statsdict
 
 def calc_fft(jsondata): #Function to calculate FFT of each sequence and add that to the JSON formatted data
         jsondata_withfft = jsondata
         return jsondata_withfft
 
-filename = 'moredata'
+filename = 'data_1'
 datafile = open(filename,'r')
 jsondata = json.load(datafile)
 """
 for buttonpress in jsondata:    ##Loop through all the button presses
         print("Button: ", buttonpress['button'])
-                 
+        print(buttonpress)
         #print("FFT:", numpy.fft(buttonpress['']));
 """
 orientationdata = convert_orientation(jsondata)
 #print(orientationdata)
-noninitialdata = remove_initial(jsondata)
+noninitialdata = remove_initial(orientationdata)
 #print("New dict:", noninitialdata)
 derivativedata = calc_derivative(noninitialdata)
 #print("New dict:", derivativedata)
 datawithdac = calc_dac(derivativedata)
 #print(datawithdac)
 statsdict = calc_stats(datawithdac)
-#print(statsdict)
+print(statsdict)
 
-"""
-for buttonpress in datawithdac:    ##Loop through all the button presses
+for buttonpress in statsdict:    ##Loop through all the button presses
         print("New button: ", buttonpress['button'])
         for key in buttonpress:
                 print(key)                
                 print(buttonpress[key])
-"""
+
 #print("All available features: ", datawithdac[0].keys())
 #print("Features per sensor reading:", len(datawithdac[0].keys()) - 2)
 
