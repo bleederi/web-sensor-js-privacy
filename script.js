6ae74568e09858c09e51e1970f2b94f7c439d09f
Jesse Nieminen <jesse.nieminen@intel.com>
2017-06-02 12:38:07 +0300

    Added license

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+/*
+ * Websensor Privacy Project
+ * https://github.com/jessenie-intel/web-sensor-js-privacy
+ *
+ * Copyright (c) 2017 Jesse Nieminen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+
+*/
+
 var output = document.querySelector('.output');
 var dataArray = [];     //array to store multiple dataObjects (one dataObject for each button press)
 var dataObject = {button:null, acceleration:null, accelerationnog:null, orientation:null, rotation:null, frequency:null};    //single reading of all the sensor data, each object a list of values obtained during button press
